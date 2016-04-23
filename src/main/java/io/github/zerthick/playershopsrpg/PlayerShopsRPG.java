0b66181ba10c98f9d3fd9a1730a2b9d9a3956e18
Zerthick <zerthick@gmail.com>
2016-04-23 15:32:38 -0400

    Add license header, update build.gradle.

--- src/main/java/io/github/zerthick/playershopsrpg/PlayerShopsRPG.java
+++ src/main/java/io/github/zerthick/playershopsrpg/PlayerShopsRPG.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2016  Zerthick
+ *
+ * This file is part of PlayerShopsRPG.
+ *
+ * PlayerShopsRPG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PlayerShopsRPG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PlayerShopsRPG.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.github.zerthick.playershopsrpg;
 
 import com.google.inject.Inject;
@@ -31,7 +50,10 @@ import org.spongepowered.api.text.chat.ChatTypes;
 import java.nio.file.Path;
 import java.util.Optional;
 
-@Plugin(id = "playershopsrpg", name = "PlayerShopsRPG", version = "0.0.1")
+@Plugin(id = "playershopsrpg",
+        name = "PlayerShopsRPG",
+        version = "0.0.1",
+        description = "A region-based player shop plugin.")
 public class PlayerShopsRPG {
 
     private ShopManager shopManager;
