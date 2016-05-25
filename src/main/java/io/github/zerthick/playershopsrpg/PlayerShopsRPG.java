817638444326f0df6099c0b014eca85e4c72e9d7
Zerthick <zerthick@gmail.com>
2016-05-25 18:15:05 -0400

    Bump plugin version number for release.

--- src/main/java/io/github/zerthick/playershopsrpg/PlayerShopsRPG.java
+++ src/main/java/io/github/zerthick/playershopsrpg/PlayerShopsRPG.java
@@ -52,7 +52,7 @@ import java.util.Optional;
 
 @Plugin(id = "playershopsrpg",
         name = "PlayerShopsRPG",
-        version = "0.1.2",
+        version = "0.1.3",
         description = "A region-based player shop plugin.")
 public class PlayerShopsRPG {
 
