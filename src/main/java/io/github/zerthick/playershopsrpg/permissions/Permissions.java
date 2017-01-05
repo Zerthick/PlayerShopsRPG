d0414e08ea64fd10ee496edad9880643fe074a07
Zerthick <zerthick@gmail.com>
2017-01-05 17:40:38 -0500

    Refactor command permission node to commands permission node

--- src/main/java/io/github/zerthick/playershopsrpg/permissions/Permissions.java
+++ src/main/java/io/github/zerthick/playershopsrpg/permissions/Permissions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016  Zerthick
+ * Copyright (C) 2017  Zerthick
  *
  * This file is part of PlayerShopsRPG.
  *
@@ -21,29 +21,29 @@ package io.github.zerthick.playershopsrpg.permissions;
 
 public class Permissions {
 
-    public static final String PLAYERSHOPSRPG_COMMAND_BUY = "playershopsrpg.command.buy";
-    public static final String PLAYERSHOPSRPG_COMMAND_RENT = "playershopsrpg.command.rent";
-    public static final String PLAYERSHOPSRPG_COMMAND_BALANCE_WITHDRAW = "playershopsrpg.command.balance.withdraw";
-    public static final String PLAYERSHOPSRPG_COMMAND_BALANCE_DEPOSIT = "playershopsrpg.command.balance.deposit";
-    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_SELL = "playershopsrpg.command.item.sell";
-    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_BUY = "playershopsrpg.command.item.buy";
-    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_REMOVE = "playershopsrpg.command.item.remove";
-    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_ADD = "playershopsrpg.command.item.add";
-    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_SET = "playershopsrpg.command.item.set";
-    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_DESTROY = "playershopsrpg.command.item.destroy";
-    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_CREATE = "playershopsrpg.command.item.create";
-    public static final String PLAYERSHOPSRPG_COMMAND_MANAGER_REMOVE = "playershopsrpg.command.manager.remove";
-    public static final String PLAYERSHOPSRPG_COMMAND_MANAGER_ADD = "playershopsrpg.command.manager.add";
-    public static final String PLAYERSHOPSRPG_COMMAND_SET_PRICE = "playershopsrpg.command.set.price";
-    public static final String PLAYERSHOPSRPG_COMMAND_SET_RENT = "playershopsrpg.command.set.rent";
-    public static final String PLAYERSHOPSRPG_COMMAND_SET_TYPE = "playershopsrpg.command.set.type";
-    public static final String PLAYERSHOPSRPG_COMMAND_SET_NAME = "playershopsrpg.command.set.name";
-    public static final String PLAYERSHOPSRPG_COMMAND_SET_OWNER = "playershopsrpg.command.set.owner";
-    public static final String PLAYERSHOPSRPG_COMMAND_SET_UNLIMITED = "playershopsrpg.command.set.unlimited";
-    public static final String PLAYERSHOPSRPG_COMMAND_BROWSE = "playershopsrpg.command.browse";
-    public static final String PLAYERSHOPSRPG_COMMAND_DESTROY = "playershopsrpg.command.destroy";
-    public static final String PLAYERSHOPSRPG_COMMAND_CREATE = "playershopsrpg.command.create";
-    public static final String PLAYERSHOPSRPG_COMMAND_SELECT = "playershopsrpg.command.select";
+    public static final String PLAYERSHOPSRPG_COMMAND_BUY = "playershopsrpg.commands.buy";
+    public static final String PLAYERSHOPSRPG_COMMAND_RENT = "playershopsrpg.commands.rent";
+    public static final String PLAYERSHOPSRPG_COMMAND_BALANCE_WITHDRAW = "playershopsrpg.commands.balance.withdraw";
+    public static final String PLAYERSHOPSRPG_COMMAND_BALANCE_DEPOSIT = "playershopsrpg.commands.balance.deposit";
+    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_SELL = "playershopsrpg.commands.item.sell";
+    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_BUY = "playershopsrpg.commands.item.buy";
+    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_REMOVE = "playershopsrpg.commands.item.remove";
+    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_ADD = "playershopsrpg.commands.item.add";
+    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_SET = "playershopsrpg.commands.item.set";
+    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_DESTROY = "playershopsrpg.commands.item.destroy";
+    public static final String PLAYERSHOPSRPG_COMMAND_ITEM_CREATE = "playershopsrpg.commands.item.create";
+    public static final String PLAYERSHOPSRPG_COMMAND_MANAGER_REMOVE = "playershopsrpg.commands.manager.remove";
+    public static final String PLAYERSHOPSRPG_COMMAND_MANAGER_ADD = "playershopsrpg.commands.manager.add";
+    public static final String PLAYERSHOPSRPG_COMMAND_SET_PRICE = "playershopsrpg.commands.set.price";
+    public static final String PLAYERSHOPSRPG_COMMAND_SET_RENT = "playershopsrpg.commands.set.rent";
+    public static final String PLAYERSHOPSRPG_COMMAND_SET_TYPE = "playershopsrpg.commands.set.type";
+    public static final String PLAYERSHOPSRPG_COMMAND_SET_NAME = "playershopsrpg.commands.set.name";
+    public static final String PLAYERSHOPSRPG_COMMAND_SET_OWNER = "playershopsrpg.commands.set.owner";
+    public static final String PLAYERSHOPSRPG_COMMAND_SET_UNLIMITED = "playershopsrpg.commands.set.unlimited";
+    public static final String PLAYERSHOPSRPG_COMMAND_BROWSE = "playershopsrpg.commands.browse";
+    public static final String PLAYERSHOPSRPG_COMMAND_DESTROY = "playershopsrpg.commands.destroy";
+    public static final String PLAYERSHOPSRPG_COMMAND_CREATE = "playershopsrpg.commands.create";
+    public static final String PLAYERSHOPSRPG_COMMAND_SELECT = "playershopsrpg.commands.select";
     public static final String PLAYERSHOPSRPG_BYPASS_OWNER = "playershopsrpg.bypass.owner";
     public static final String PLAYERSHOPSRPG_BYPASS_MANAGER = "playershopsrpg.bypass.manager";
 }
