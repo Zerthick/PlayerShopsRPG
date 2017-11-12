272f2d0712a27165a3e73ef1ab56b0ba124f97c1
Zerthick <zerthick@gmail.com>
2017-11-12 16:56:41 -0500

    Mark as dev build

--- src/main/java/io/github/zerthick/playershopsrpg/PlayerShopsRPG.java
+++ src/main/java/io/github/zerthick/playershopsrpg/PlayerShopsRPG.java
@@ -53,7 +53,7 @@ import java.util.Optional;
 
 @Plugin(id = "playershopsrpg",
         name = "PlayerShopsRPG",
-        version = "2.0.5",
+        version = "0.2.0",
         description = "A region-based player shop plugin.",
         authors = {
                 "Zerthick"
